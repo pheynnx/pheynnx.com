e9b1ebeaa28a7f1d4ebbca5df139996fbbf4f792
Eric Christensen <hebrew.8.12@gmail.com>
2024-07-04 11:35:29 -0700

    updated build script

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+LOG_FILE="pheynnx.com.log"
+APP_NAME="pheynnx.com"
+
+get_pid() {
+    echo $(ps aux | grep "$APP_NAME" | grep -v "grep" | awk '{print $2}')
+}
+
 if [ "$1" == "build" ]; then
 
     echo "Compiling SCSS files..."
@@ -29,6 +36,30 @@ elif [ "$1" == "dev" ]; then
 
     echo "Development environment setup completed."
 
+elif [ "$1" == "prod" ]; then
+    echo "Compiling SCSS files..."
+    sass --no-source-map --style=compressed web/source/scss:web/static/css
+
+    echo "Generating templ files..."
+    templ generate
+
+    echo "Building Go project..."
+    go build -o build/pheynnx.com cmd/pheynnx/main.go
+
+    echo "Running pheynnx.com headless"
+
+    if [ ! -f build/pheynnx.com ]; then
+        echo "Go binary not found. Building Go project..."
+        go build -o build/pheynnx.com path/to/your/main.go
+    fi
+
+    nohup build/pheynnx.com > "$LOG_FILE" 2>&1 &
+
+    echo "Go project is running headless. Output is redirected to $LOG_FILE."
+
+    PID=$(get_pid)
+    echo "Go project PID: $PID"
+    
 else
-    echo "Invalid argument. Please use 'build' or 'dev'."
+    echo "Invalid argument. Please use 'build' | 'dev' | 'prod'."
 fi
